6abde8f743f2a93c8204f820a46c135ae4111b59
kirion13 <kirioncore@gmail.com>
2022-05-17 19:53:41 +0200

    End

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const list = document.getElementById("navbar__list");
 const sections = document.querySelectorAll("section");
-
+const conter = document.querySelectorAll(".landing__container");
+console.log(conter);
 const markup = [...sections]
   .map(
     (item) =>
@@ -17,3 +18,18 @@ list.addEventListener("click", (e) => {
 list.style.cssText =
   " min-height:50px; width:100%; background:#273c75; display:flex; justify-content:center; align-items:center; border-bottom:5px solid #04aa6d; text-align:center;";
 ///
+const active = document.querySelectorAll(".menu__link");
+window.addEventListener("scroll", function () {
+  sections.forEach((section, i) => {
+    if (
+      section.getBoundingClientRect().top <= 27 &&
+      section.getBoundingClientRect().bottom >= 27
+    ) {
+      section.classList.add("your-active-class");
+      active[i].classList.add("active");
+    } else {
+      section.classList.remove("your-active-class");
+      active[i].classList.remove("active");
+    }
+  });
+});
